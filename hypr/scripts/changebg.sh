eb6775933b5dc78cad11848a0aeaf4d1405932cf
nez6-dev <pramim7antofaz@gmail.com>
2025-12-25 11:48:51 -0300

    1.2 exclusive hypr decoration file

--- hypr/scripts/changebg.sh
+++ hypr/scripts/changebg.sh
@@ -6,6 +6,5 @@ if [ "$?" -eq "0" ]; then
   swww-daemon & disown
 else
   killall swww-daemon
-  mpvpaper -o loop ALL ~/Imagens/Wallpapers/dedsec-rice/dedsec.mp4 & disown
-#  mpvpaper -o loop ALL ~/Imagens/Wallpapers/dedsec-rice/dedsec2.mp4 & diswon
+  mpvpaper -o loop ALL ~/Imagens/Wallpapers/ricePapers/dedsec.mp4 & disown
 fi
